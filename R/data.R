7562cb97698752f47d8b3843e3ba1ba179de5697
cbedwards <edwards.evoeco@gmail.com>
2021-03-01 18:09:57 -0500

    Modified data approach: now in usable data frame form (sour.data) with handy map to species names (spec.map), and reasonable documentation (?sour.data, ?spec.map).

--- R/data.R
+++ R/data.R
@@ -1,16 +1,33 @@
 #' Sourdough Data
 #'
-#' A dataset containing results of competition experiments of sourdough communities from Landis et al. 2020.o
+#' A data frame of timeseries data from pairwise competition experiments of sourdough communities from Landis et al. 2020.
 #'
-#' @format A list with seven sub-lists.
+#' Note from the data file of Landis et al:
+#'   "CFU counts and relative abundance data from competitions, transfers 1, 3, and 6 - Pairs were transferred every 48 hours through filtered flour media (see materials and methods)."
+#'
+#' @format A data frame with 1080 rows and 6 variables.
 #' \describe{
-#'   \item{RA.xfer.1}{Relative abundance data at transfer 1.}
-#'   \item{RA.xfer.3}{Relative abundance data at transfer 3.}
-#'   \item{RA.xfer.6}{Relative abundance data at transfer 6.}
-#'   \item{RA.xfer.1}{absolute abundance data at transfer 2.}
-#'   \item{RA.xfer.3}{Relative abundance data at transfer 3.}
-#'   \item{RA.xfer.6}{Relative abundance data at transfer 6.}
-#'   \item{readme}{map of species numbers and letters to species names.}
+#'   \item{abund}{Are the values relative ("rel") or absolute ("abs") abundance?}
+#'   \item{transf}{What is the transfer number? Multiply by 48 for hours since experiment start}
+#'   \item{spec1}{Identifier of first competing species. See "spec.map" to get genus/species from the identifier}
+#'   \item{spec2}{Identifier of second competing species.}
+#'   \item{abund1}{relative or absolute abundance of species 1}
+#'   \item{abund2}{relative or absolute abundance of species 2}
 #' }
-#' @source \url{https://elifesciences.org/articles/61644#content}
-"sour_data"
+#' @source \url{https://elifesciences.org/articles/61644}
+#' @seealso spec.map
+"sour.data"
+
+
+#' Map of sourdough species
+#'
+#' A companion to sour.data, matching species ID to the genus and species name.
+
+#' @format A data frame with 8 rows and 2 variables.
+#' \describe{
+#'   \item{name.data}{Identifier used in sour.data. Note that numbers correspond to fungi, letters to bacteria.}
+#'   \item{name.full}{Scientific name}
+#' }
+#' @source \url{https://elifesciences.org/articles/61644}
+#' @seealso sour.data
+"spec.map"
--- R/sourdough_data.R
+++ R/sourdough_data.R
@@ -1,69 +1,73 @@
-library(readxl)
+library(tidyverse)
+library(readxl) ## apparently this is bad practice?
+## but I'm not sure how it knows about the package otherwise.
+## leaving it in for now.
 
 ## code to prepare `wine_data` dataset goes here
 # retrieve paths to datafiles
-first.file <- system.file(
+filename <- system.file(
   "extdata",
   "Figure_3_-_Source_Data_1.xlsx",
   package = "SourCoex"
 )
 
 # read the two .csv files
-dat.readme= read_excel(
-  first.file
+spec.map= read_excel(#this tab was added by me, modified from the Readme tab for easier formatting.
+  filename,
+  sheet="speciesMap"
 )
 
-dat.xfer1RA= read_excel(path=first.file,
+dat.xfer1RA= read_excel(path=filename,
                          sheet="xfer1RA"
 )
-dat.xfer1RA.ls=list(time="first transfer",
-                    abund="relative",
-                    data=dat.xfer1RA)
-dat.xfer3RA= read_excel(path=first.file,
+dat.xfer3RA= read_excel(path=filename,
                         sheet="xfer3RA"
 )
-dat.xfer3RA.ls=list(time="third transfer",
-                    abund="relative",
-                    data=dat.xfer3RA)
-
-dat.xfer6RA= read_excel(path=first.file,
+dat.xfer6RA= read_excel(path=filename,
                         sheet="xfer6RA"
 )
-dat.xfer6RA.ls=list(time="sixth transfer",
-                    abund="relative",
-                    data=dat.xfer6RA)
-
-dat.xfer1AA= read_excel(path=first.file,
+dat.xfer1AA= read_excel(path=filename,
                         sheet="xfer1AA"
 )
-dat.xfer1AA.ls=list(time="first transfer",
-                    abund="absolute",
-                    data=dat.xfer1AA)
-
-dat.xfer3AA= read_excel(path=first.file, ##Note: Dryad includes a random "2" about 40 cols in
+dat.xfer3AA= read_excel(path=filename, ##Note: Dryad includes a random "2" about 40 cols in
                         ## I have removed this in the excel file.
                         sheet="xfer3AA"
 )
-dat.xfer3AA.ls=list(time="third transfer",
-                    abund="absolute",
-                    data=dat.xfer3AA)
-
-dat.xfer6AA= read_excel(path=first.file,
+dat.xfer6AA= read_excel(path=filename,
                         ##Note: Dryad includes a random "2" about 40 cols in
                         ## I have removed this in the excel file.
                         sheet="xfer6AA"
 )
-dat.xfer6AA.ls=list(time="sixth transfer",
-                    abund="absolute",
-                    data=dat.xfer6AA)
 
-sour_data = list(RA.xfer.1=dat.xfer1RA.ls,
-                 RA.xfer.3=dat.xfer3RA.ls,
-                 RA.xfer.6=dat.xfer6RA.ls,
-                 AA.xfer.1=dat.xfer1AA.ls,
-                 AA.xfer.3=dat.xfer3AA.ls,
-                 AA.xfer.6=dat.xfer6AA.ls,
-                 readme=dat.readme)
+#helper function: restructuring from the specific structure given in Dryad.
+restruc_ = function(data, #data is the original data from Landis et al sheet.
+                    abund, #abund is the type of abundance ("rel" or "abs" for relative or absolut)
+                    transf #which transfer was it? Numeric.
+                    ){
+  temp=rbind(select(data, Sp1, Sp2, starts_with("R1"))%>%
+          rename(spec1=1, spec2=2, abund1=3, abund2=4),
+        select(data, Sp1, Sp2, starts_with("R1"))%>%
+          rename(spec1=1, spec2=2, abund1=3, abund2=4),
+        select(data, Sp1, Sp2, starts_with("R3"))%>%
+          rename(spec1=1, spec2=2, abund1=3, abund2=4),
+        select(data, Sp1, Sp2, starts_with("R4"))%>%
+          rename(spec1=1, spec2=2, abund1=3, abund2=4),
+        select(data, Sp1, Sp2, starts_with("R5"))%>%
+          rename(spec1=1, spec2=2, abund1=3, abund2=4))
+  temp = cbind(temp,
+               abund="rel",
+               transf=1)
+  return(temp)
+}
 
+sour.data=rbind(restruc_(dat.xfer1RA, abund="rel", transf=1),
+               restruc_(dat.xfer3RA, abund="rel", transf=3),
+               restruc_(dat.xfer6RA, abund="rel", transf=6),
+               restruc_(dat.xfer1AA, abund="abs", transf=1),
+               restruc_(dat.xfer3AA, abund="abs", transf=3),
+               restruc_(dat.xfer6AA, abund="abs", transf=6)
+)
+sour.data=sour.data %>%
+  relocate(abund, transf, spec1, spec2, abund1, abund2)
 # save the wine_data dataframe as an .rda file in WineReviews/data/
-usethis::use_data(sour_data, overwrite = TRUE)
+usethis::use_data(sour.data, spec.map, overwrite = TRUE)
